e09a183c3b6af7e8b9073eb23747274652ab24de
kkasior <karol.kasior@gmail.com>
2020-08-22 12:03:26 +0200

    Created test for url verification of the DevTo main page

--- src/test/java/FirstTests.java
+++ src/test/java/FirstTests.java
@@ -3,17 +3,24 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static org.junit.Assert.assertEquals;
+
 public class FirstTests {
 
     WebDriver driver;
 
     @Before
-    public void Setup(){
+    public void setup(){
         System.setProperty("webdriver.chrome.driver","C:\\chromedriver\\chromedriver.exe");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(); //otworz nową instancję chrome
+        driver.manage().window().maximize(); //maksymalizuj przeglądarkę
     }
     @Test
-    public void firstTest(){
-
+    public void verifyDevToUrl(){
+        String devToUrl = "https://dev.to";
+        driver.get(devToUrl);
+        String currentUrl = driver.getCurrentUrl();
+        assertEquals("urls aren't the same",devToUrl,currentUrl);
     }
+    
 }
