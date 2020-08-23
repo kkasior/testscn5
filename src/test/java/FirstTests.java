fe3c3a9187faf7535c5be6fb23d955bac517258d
kkasior <karol.kasior@gmail.com>
2020-08-23 09:12:11 +0200

    blog test implementation

--- src/test/java/FirstTests.java
+++ src/test/java/FirstTests.java
@@ -1,14 +1,17 @@
 import org.junit.Before;
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+        import org.junit.Test;
+        import org.openqa.selenium.By;
+        import org.openqa.selenium.JavascriptExecutor;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assert.assertEquals;
+        import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class FirstTests {
 
@@ -35,9 +38,28 @@ public class FirstTests {
     @Test
     public void goToPodcastsAndOpenFirstPodcast(){
         WebElement podcastsButton = driver.findElement(By.xpath("//a[@href='/pod']"));
-        //highLightElement(driver,podcastsButton);
         podcastsButton.click();
         WebElement firstPodcast = driver.findElement(By.className("content"));
-        highLightElement(driver,firstPodcast);
+        //firstPodcast.click();
+        WebElement firstPodcastText = driver.findElement(By.cssSelector(".content > h3:first-child"));
+        highLightElement(driver,firstPodcastText);
+        String firstPodcastTextValue = firstPodcastText.getText();
+        firstPodcastTextValue = firstPodcastTextValue.substring(7);
+        firstPodcast.click();
+        WebElement podcastTitle = driver.findElement(By.cssSelector(".title > h1:nth-child(2)"));
+        highLightElement(driver,podcastTitle);
+        String podcastTitleText = podcastTitle.getText();
+
+        assertEquals("script entered wrong podcast",firstPodcastTextValue,podcastTitleText);
+
+        WebElement recordButton = driver.findElement(By.className("record-wrapper"));
+        recordButton.click();
+
+        WebDriverWait wait=new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.className("status-message"))));
+
+        String classAttributeOfRecordButton = recordButton.getAttribute("class");
+
+        assertTrue("Podcast isn't played",classAttributeOfRecordButton.contains("playing"));
     }
 }
